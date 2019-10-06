954afa9e71bb75fce80aba760f371dc92d2559db
AJAberdein <ashley@aberdein.co.za>
2019-10-06 07:32:53 +0200

    Create a test for the end case of the whole rover movement feature. -This is the final test that should pass when all other aspects of the application is implemented. - ie. This should fail until the very end when all the functionality is created.
    Next: make a test for creating a new Map object.

---
+++ tests/Feature/MarsRoverAnalysisTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\MarsRoverAnalysis;
+
+class MarsRoverAnalysisTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function a_single_mars_rover_can_navigate_a_map()
+    {
+
+        $input = "
+            5 5
+            1 2 N
+            LMLMLMLMM
+        ";
+
+        $rover_analyser = new MarsRoverAnalysis();
+        $result = $rover_analyser->assess($input);
+
+        $this->assertEquals("1 3 N", $result);
+    }
+
+    /**
+     * @test
+     */
+    public function two_mars_rover_can_navigate_a_map()
+    {
+        $input = "
+            5 5
+            1 2 N
+            LMLMLMLMM
+            3 3 E
+            MMRMMRMRRM
+        ";
+
+        $rover_analyser = new MarsRoverAnalysis();
+        $result = $rover_analyser->assess($input);
+
+        $this->assertEquals("1 3 N\n5 1 E", $result);
+
+    }
+}
